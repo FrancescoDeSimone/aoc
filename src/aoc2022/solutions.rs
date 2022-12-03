0eb974fc0ce527a455d718ea085f57b583cb942c
fdesi <francesco.desimone@canonical.com>
2022-12-03 22:34:08 +0100

    day3

--- src/aoc2022/solutions.rs
+++ src/aoc2022/solutions.rs
@@ -1,4 +1,5 @@
 use std::collections::{BinaryHeap, HashMap};
+use regex::Regex;
 
 pub fn day1_1(input: String) -> usize {
     input
@@ -43,7 +44,7 @@ pub fn day1_2(input: String) -> usize {
         .into_vec()
         .into_iter()
         .take(3)
-        .sum::<usize>()
+    .sum::<usize>()
 }
 
 pub fn day2_1(input: String) -> usize {
@@ -51,7 +52,7 @@ pub fn day2_1(input: String) -> usize {
         ("A", HashMap::from([("Y", 8), ("X", 4), ("Z", 3)])),
         ("B", HashMap::from([("Y", 5), ("X", 1), ("Z", 9)])),
         ("C", HashMap::from([("Y", 2), ("X", 7), ("Z", 6)])),
-    ]);
+        ]);
     let mut score = 0;
     for game in input.split('\n').filter(|x| !x.is_empty()) {
         let (enemy, me) = game.split_at(game.find(' ').unwrap());
@@ -65,11 +66,48 @@ pub fn day2_2(input: String) -> usize {
         ("A", HashMap::from([("Y", 4), ("X", 3), ("Z", 8)])),
         ("B", HashMap::from([("Y", 5), ("X", 1), ("Z", 9)])),
         ("C", HashMap::from([("Y", 6), ("X", 2), ("Z", 7)])),
-    ]);
+        ]);
     let mut score = 0;
     for game in input.split('\n').filter(|x| !x.is_empty()) {
         let (enemy, me) = game.split_at(game.find(' ').unwrap());
         score += points_table[enemy.trim()][me.trim()];
     }
     score
+}
+
+pub fn day3_1(input: String) -> usize {
+    let mut priority: usize = 0;
+    for rucksacks in input.split('\n').filter(|x| !x.is_empty()) {
+        let (sack_l, sack_r) = rucksacks.split_at(rucksacks.len() / 2);
+        for e in sack_l.chars() {
+            if sack_r.contains(e) {
+                if e.is_lowercase() {
+                    priority += 1 + (e as usize - 'a' as usize);
+                } else {
+                    priority += 27 + (e as usize - 'A' as usize);
+                }
+                break;
+            }
+        }
+    }
+    priority
+}
+
+pub fn day3_2(input: String) -> usize {
+    let mut priority: usize = 0;
+    let group_regex = Regex::new(r"(?m).*?\n.*?\n.*?\n").unwrap();
+    for group in group_regex.captures_iter(&input) {
+        let rucksacks = group.get(0).unwrap().as_str().split('\n').filter(|x| !x.is_empty()).collect::<Vec<_>>();
+        for c in rucksacks[0].chars() {
+            if rucksacks[1].contains(c) && rucksacks[2].contains(c) {
+                if c.is_lowercase() {
+                    priority += 1 + (c as usize - 'a' as usize);
+                } else {
+                    priority += 27 + (c as usize - 'A' as usize);
+                }
+                break;
+            }
+        }
+    }
+    priority
 }
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@ fn main() {
     println!("Part 1: {}", solutions::day1_1(file.clone()));
     println!("Part 2: {}", solutions::day1_2(file.clone()));
 
-
     println!("Day 2");
     let file = fs::read_to_string("src/aoc2022/day2").expect("day2 file not found");
     println!("Part 1: {}", solutions::day2_1(file.clone()));
     println!("Part 2: {}", solutions::day2_2(file.clone()));
+
+    println!("Day 3");
+    let file = fs::read_to_string("src/aoc2022/day3").expect("day3 file not found");
+    println!("Part 1: {}", solutions::day3_1(file.clone()));
+    println!("Part 2: {}", solutions::day3_2(file.clone()));
 }
